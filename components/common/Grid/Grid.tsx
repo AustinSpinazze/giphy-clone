860f38a94e76c6a5caa3c01c07353662d6f2ce37
AustinSpinazze <aspinazze94@gmail.com>
2022-11-17 21:18:49 -0600

    Fix #7

--- components/common/Grid/Grid.tsx
+++ components/common/Grid/Grid.tsx
@@ -2,7 +2,6 @@ import { GridProps } from '../../../utils/types/types';
 import styles from './styles.module.css';
 
 const Grid = ({ gifs, Card, columns }: GridProps) => {
-	console.log(gifs);
 	return (
 		<ul className={`${styles.grid} ${styles[columns]}`}>
 			{gifs.map((gif, index) => {
