b65b40a63e29ebb03903f5466ac2e7218109ee58
AustinSpinazze <aspinazze94@gmail.com>
2022-11-17 21:12:17 -0600

    logging grid

--- components/common/Grid/Grid.tsx
+++ components/common/Grid/Grid.tsx
@@ -2,6 +2,7 @@ import { GridProps } from '../../../utils/types/types';
 import styles from './styles.module.css';
 
 const Grid = ({ gifs, Card, columns }: GridProps) => {
+	console.log(gifs);
 	return (
 		<ul className={`${styles.grid} ${styles[columns]}`}>
 			{gifs.map((gif, index) => {
